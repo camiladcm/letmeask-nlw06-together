90b8664053972cb41c3de3418fa91976bbc3a693
cmarcal <camiladcmarcal@gmail.com>
2021-06-26 22:56:56 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { AuthContextProvider } from './contexts/AuthContext';
+
+import { Home } from './components/Home';
+import { NewRoom } from './components/NewRoom';
+import { Room } from './components/Room';
+import { AdminRoom } from './components/AdminRoom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
+        </Switch>
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
